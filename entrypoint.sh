a801a872890a6e0435d5fdc5c73a9da04d2bd7cd
Laxmi Devi <40895799+laxmibaghel@users.noreply.github.com>
2024-07-19 17:49:55 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,24 @@
-#!bin/bash
+#!/bin/bash
 
 echo "****************************************************************"
 
+# Configure Git
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
-git config -global --add safe.directory /github/workspace
+git config --global --add safe.directory /github/workspace
 
+# Run the Python script
 python3 /usr/bin/feed.py
 
-git add -A && git commit -m "Update feed" && git push --set-upstream origin main
+# Add all changes to the staging area
+git add -A
 
+# Check if there are changes to commit
+if git diff-index --quiet HEAD; then
+  echo "No changes to commit"
+else
+  git commit -m "Update feed"
+  git push --set-upstream origin main
+fi
 
 echo "****************************************************************"
-
